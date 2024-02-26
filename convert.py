6345ce15af99c0e5c3dd9d0d7879535daa8af955
sbl <sbl@brienz.ch>
2024-02-26 17:01:57 +0100

    Updated convert.py
    Added functionality for gradient backgrounds

--- convert.py
+++ convert.py
@@ -4,7 +4,7 @@ from multiprocessing.pool import ThreadPool as Pool
 os.environ['path'] += r';C:\Program Files\Inkscape\bin'
 
 from cairosvg import svg2png
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 
 
 ## define your wanted color variations here
@@ -17,7 +17,11 @@ variants = [
     ["pink2", (235, 82, 148), (11, 27, 56)],
     ["yellow", (247, 206, 0), (11, 27, 56)],
     ["red", (247, 0, 0), (11, 27, 56)],
-    ["white", (252, 252, 252), (11, 27, 56)]
+    ["white", (252, 252, 252), (11, 27, 56)],
+    ["nvidia", (255,255,255), [
+        (123,185,7),
+        (55,141,52)
+    ]]
 ]
 
 ## settings
@@ -36,6 +40,11 @@ if not os.path.exists("tmp"):
 # get all svg files from input
 items = glob.glob("input/*.svg")
 
+def interpolate(f_co, t_co, interval):
+    det_co =[(t - f) / interval for f , t in zip(f_co, t_co)]
+    for i in range(interval):
+        yield [round(f + det * i) for f, det in zip(f_co, det_co)]
+
 # created the final icon
 def createIcon(icon, color, backgroundColor = (11, 27, 56)):
     # Foreground
@@ -43,9 +52,15 @@ def createIcon(icon, color, backgroundColor = (11, 27, 56)):
     foreground.putalpha(icon)
 
     # Background
-    background  = Image.new( mode = "RGBA", size = (width, height), color = backgroundColor )
-    background.paste(foreground, (24, 8), foreground)
+    if type(backgroundColor) == list:
+        background  = Image.new( mode = "RGBA", size = (width, height), color = 0 )
+        draw = ImageDraw.Draw(background)
+        for i, color in enumerate(interpolate(backgroundColor[0], backgroundColor[1], background.width * 2)):
+            draw.line([(0, i), (background.width, i)], tuple(color), width=1)
+    else:
+        background  = Image.new( mode = "RGBA", size = (width, height), color = backgroundColor )
 
+    background.paste(foreground, (24, 8), foreground)
     background = background.convert("P", palette=Image.ADAPTIVE, colors=64)
 
     return background
