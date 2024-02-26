ddfafb26a9abdeaa0e60d558dc9024d05a5f7d83
Gummientchen <steff.freak@gmail.com>
2024-02-26 13:09:41 +0100

    added functionality to define color variants easily

--- convert.py
+++ convert.py
@@ -6,6 +6,20 @@ os.environ['path'] += r';C:\Program Files\Inkscape\bin'
 from cairosvg import svg2png
 from PIL import Image, ImageOps
 
+
+## define your wanted color variations here
+variants = [
+    ["aqua", (0, 200, 253), (11, 27, 56)],
+    ["blue", (59, 123, 255), (11, 27, 56)],
+    ["green", (47, 199, 134), (11, 27, 56)],
+    ["orange", (255, 109, 76), (11, 27, 56)],
+    ["pink", (194, 89, 240), (11, 27, 56)],
+    ["pink2", (235, 82, 148), (11, 27, 56)],
+    ["yellow", (247, 206, 0), (11, 27, 56)],
+    ["red", (247, 0, 0), (11, 27, 56)]
+]
+
+## settings
 pool_size = 16  # your "parallelness"
 width = 144
 height = 144
@@ -21,6 +35,7 @@ if not os.path.exists("tmp"):
 # get all svg files from input
 items = glob.glob("input/*.svg")
 
+# created the final icon
 def createIcon(icon, color, backgroundColor = (11, 27, 56)):
     # Foreground
     foreground  = Image.new( mode = "RGB", size = (96, 96), color = color )
@@ -34,6 +49,7 @@ def createIcon(icon, color, backgroundColor = (11, 27, 56)):
 
     return background
 
+# prepares the icon vor generation
 def createPNGfromSVG(svgFilename):
     filename = os.path.basename(svgFilename)
     filename = os.path.splitext(filename)[0]
@@ -43,14 +59,6 @@ def createPNGfromSVG(svgFilename):
     # create output folder
     if not os.path.exists(outputFolder):
         os.makedirs(outputFolder)
-
-    # Output Filenames for color variants
-    outputFilenameAqua = "".join([outputFolder, "/", filename, "-aqua.png"])
-    outputFilenameBlue = "".join([outputFolder, "/", filename, "-blue.png"])
-    outputFilenameGreen = "".join([outputFolder, "/", filename, "-green.png"])
-    outputFilenameOrange = "".join([outputFolder, "/", filename, "-orange.png"])
-    outputFilenamePink = "".join([outputFolder, "/", filename, "-pink.png"])
-    outputFilenamePink2 = "".join([outputFolder, "/", filename, "-pink2.png"])
 
     # convert SVG to PNG if neccessary
     if not os.path.isfile(tmpFilename):
@@ -65,25 +73,12 @@ def createPNGfromSVG(svgFilename):
     iconInverted = ImageOps.invert(iconRGB).convert("L")
 
     # Color variants
-    iconAqua = createIcon(iconInverted, (0, 200, 253))
-    iconAqua.save(outputFilenameAqua, optimize = True)
+    for variant in variants:
+        iconVariant = createIcon(iconInverted, variant[1], variant[2])
+        outputFilename = "".join([outputFolder, "/", filename, "-",variant[0],".png"])
+        iconVariant.save(outputFilename, optimize = True)
 
-    iconBlue = createIcon(iconInverted, (59, 123, 255))
-    iconBlue.save(outputFilenameBlue, optimize = True)
-
-    iconGreen = createIcon(iconInverted, (47, 199, 134))
-    iconGreen.save(outputFilenameGreen, optimize = True)
-
-    iconOrange = createIcon(iconInverted, (255, 109, 76))
-    iconOrange.save(outputFilenameOrange, optimize = True)
-
-    iconPink = createIcon(iconInverted, (194, 89, 240))
-    iconPink.save(outputFilenamePink, optimize = True)
-
-    iconPink2 = createIcon(iconInverted, (235, 82, 148))
-    iconPink2.save(outputFilenamePink2, optimize = True)
-
-
+#multithreading
 pool = Pool(pool_size)
 for item in items:
     pool.apply_async(createPNGfromSVG, (item,))
