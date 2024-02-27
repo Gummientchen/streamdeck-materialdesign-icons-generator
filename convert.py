dad823ab82f9079836643a71a872d96fdbb237ec
sbl <sbl@brienz.ch>
2024-02-27 17:36:14 +0100

    Updated convert.py
    Refactored the code and added error handling

--- convert.py
+++ convert.py
@@ -1,11 +1,17 @@
 import os
 import glob
+import math
+import time
+
+from tqdm import tqdm
+
 from multiprocessing.pool import ThreadPool as Pool
 os.environ['path'] += r';C:\Program Files\Inkscape\bin'
 
 from cairosvg import svg2png
 from PIL import Image, ImageOps, ImageDraw
 
+start_time = time.time()
 
 ## define your wanted color variations here
 variants = [
@@ -26,6 +32,8 @@ pool_size = 16  # your "parallelness"
 width = 144
 height = 144
 
+final_size = 72, 72
+
 # create directories
 if not os.path.exists("input"):
     os.makedirs("input")
@@ -42,6 +50,29 @@ def interpolate(f_co, t_co, interval):
     for i in range(interval):
         yield [round(f + det * i) for f, det in zip(f_co, det_co)]
 
+# create animated frames
+def createAnimatedIcon(name, foreground, background, duration = 1000, backgroundColor = (200,0,0), frames = 4):
+    icon = Image.new( mode = "RGBA", size = (width, height), color = backgroundColor )
+    icon.paste(foreground, (24, 8), foreground)
+
+    animatedframes = []
+
+
+    for f in range(frames):
+        frame = Image.blend(background, icon, 1/frames*(f+1))
+        frame.thumbnail(final_size, Image.Resampling.LANCZOS)
+        frame = frame.convert("P", palette=Image.ADAPTIVE, colors=64)
+        animatedframes.append(frame)
+    
+    animation = {
+        "name": name,
+        "duration": duration,
+        "frames": animatedframes
+    }
+
+    return animation
+
+
 # created the final icon
 def createIcon(icon, color, backgroundColor = (11, 27, 56)):
     # Foreground
@@ -57,48 +88,20 @@ def createIcon(icon, color, backgroundColor = (11, 27, 56)):
     else:
         background  = Image.new( mode = "RGBA", size = (width, height), color = backgroundColor )
     
-    # Background animated
-    animatedRed = Image.new( mode = "RGBA", size = (width, height), color = (200,0,0) )
-    animatedGreen = Image.new( mode = "RGBA", size = (width, height), color = (0,220,0) )
-
     # composite icon
     background.paste(foreground, (24, 8), foreground)
-    animatedRed.paste(foreground, (24, 8), foreground)
-    animatedRed1 = Image.blend(background, animatedRed, 0.25)
-    animatedRed2 = Image.blend(background, animatedRed, 0.5)
-    animatedRed3 = Image.blend(background, animatedRed, 0.75)
-    animatedGreen.paste(foreground, (24, 8), foreground)
-    animatedGreen1 = Image.blend(background, animatedGreen, 0.25)
-    animatedGreen2 = Image.blend(background, animatedGreen, 0.5)
-    animatedGreen3 = Image.blend(background, animatedGreen, 0.75)
     
-
-    # reduce colors
+    # define animated versions here
+    animations = []
+    animations.append(createAnimatedIcon("red", foreground, background, 1000, (200,0,0), 8))
+    animations.append(createAnimatedIcon("green", foreground, background, 1000, (0,220,0), 8))
+    animations.append(createAnimatedIcon("orange", foreground, background, 1000, (255, 109, 76), 8))
+    
+    # scale down and reduce colors
+    background.thumbnail(final_size, Image.Resampling.LANCZOS)
     background = background.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedRed = animatedRed.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedRed1 = animatedRed1.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedRed2 = animatedRed2.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedRed3 = animatedRed3.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedGreen = animatedGreen.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedGreen1 = animatedGreen1.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedGreen2 = animatedGreen2.convert("P", palette=Image.ADAPTIVE, colors=64)
-    animatedGreen3 = animatedGreen3.convert("P", palette=Image.ADAPTIVE, colors=64)
 
-    animatedRedIcon = [
-        animatedRed,
-        animatedRed1,
-        animatedRed2,
-        animatedRed3
-    ]
-
-    animatedGreenIcon = [
-        animatedGreen,
-        animatedGreen1,
-        animatedGreen2,
-        animatedGreen3
-    ]
-
-    return background, animatedRedIcon, animatedGreenIcon
+    return background, animations
 
 # prepares the icon vor generation
 def createPNGfromSVG(svgFilename):
@@ -106,16 +109,10 @@ def createPNGfromSVG(svgFilename):
     filename = os.path.splitext(filename)[0]
     tmpFilename = "".join(["tmp/", filename, ".png"])
     outputFolder = "".join(["output/", filename])
-    outputFolderAnimatedRed = "".join(["output/", filename, "/animated-red"])
-    outputFolderAnimatedGreen = "".join(["output/", filename, "/animated-green"])
 
     # create output folder
     if not os.path.exists(outputFolder):
         os.makedirs(outputFolder)
-    if not os.path.exists(outputFolderAnimatedRed):
-        os.makedirs(outputFolderAnimatedRed)
-    if not os.path.exists(outputFolderAnimatedGreen):
-        os.makedirs(outputFolderAnimatedGreen)
 
     # convert SVG to PNG if neccessary
     if not os.path.isfile(tmpFilename):
@@ -123,56 +120,63 @@ def createPNGfromSVG(svgFilename):
             url=svgFilename, scale=4, write_to=tmpFilename)
     
     # Icon
-    icon = Image.open(tmpFilename)
-    iconBackground  = Image.new( mode = "RGBA", size = (96, 96), color = (255, 255, 255) )
-    iconCombined = Image.alpha_composite(iconBackground, icon)
-    iconRGB = iconCombined.convert("RGB")
-    iconInverted = ImageOps.invert(iconRGB).convert("L")
+    try:
+        icon = Image.open(tmpFilename)
+        iconBackground  = Image.new( mode = "RGBA", size = (96, 96), color = (255, 255, 255) )
+        iconCombined = Image.alpha_composite(iconBackground, icon)
+        iconRGB = iconCombined.convert("RGB")
+        iconInverted = ImageOps.invert(iconRGB).convert("L")
+    except ValueError:
+        print("images do not match:", svgFilename)
+        return False
 
     # Color variants
     for variant in variants:
-        iconVariant, iconVariantAnimatedRed, iconVariantAnimatedGreen = createIcon(iconInverted, variant["color"], variant["background"])
-
         outputFilename = "".join([outputFolder, "/", filename, "-",variant["name"],".png"])
-        outputFilenameAnimatedRed = "".join([outputFolderAnimatedRed, "/", filename, "-",variant["name"],"-animated-red.gif"])
-        outputFilenameAnimatedGreen = "".join([outputFolderAnimatedGreen, "/", filename, "-",variant["name"],"-animated-green.gif"])
 
-        iconVariant.save(outputFilename, optimize = True)
-        iconVariant.save(
-            outputFilenameAnimatedRed,
-            optimize = False,
-            save_all=True,
-            append_images=[
-                iconVariant,
-                iconVariant,
-                iconVariant,
-                iconVariantAnimatedRed[1],
-                iconVariantAnimatedRed[2],
-                iconVariantAnimatedRed[3],
-                iconVariantAnimatedRed[0]
-                ],
-            duration=125,
-            loop=0)
-        iconVariant.save(
-            outputFilenameAnimatedGreen,
-            optimize = False,
-            save_all=True,
-            append_images=[
-                iconVariant,
-                iconVariant,
-                iconVariant,
-                iconVariantAnimatedGreen[1],
-                iconVariantAnimatedGreen[2],
-                iconVariantAnimatedGreen[3],
-                iconVariantAnimatedGreen[0]
-                ],
-            duration=125,
-            loop=0)
+        if not os.path.isfile(outputFilename):
+            iconVariant, animations = createIcon(iconInverted, variant["color"], variant["background"])
+            iconVariant.save(outputFilename, optimize = True)
 
-#multithreading
-pool = Pool(pool_size)
-for item in items:
-    pool.apply_async(createPNGfromSVG, (item,))
+            for animation in animations:
+                outputFolderAnimated = "".join(["output/", filename, "/animated-", animation["name"]])
+                if not os.path.exists(outputFolderAnimated):
+                    os.makedirs(outputFolderAnimated)
+                outputFilenameAnimated = "".join([outputFolderAnimated, "/", filename, "-",variant["name"],"-animated-",animation["name"],".gif"])
+                
+                images = []
 
-pool.close()
-pool.join()
+                for frame in animation["frames"]:
+                    images.append(frame)
+
+                for img in range(len(animation["frames"])-1):
+                    images.append(iconVariant)
+
+                duration = math.floor(animation["duration"] / (len(animation["frames"])*2))
+
+
+                iconVariant.save(
+                    outputFilenameAnimated,
+                    optimize = True,
+                    save_all=True,
+                    append_images=images,
+                    duration=duration,
+                    loop=0)
+
+def main():
+    #multithreading
+    # pool = Pool(pool_size)
+
+    # for item in items:
+    #     pool.apply_async(createPNGfromSVG, (item,))
+
+    for item in tqdm(items):
+        createPNGfromSVG(item)
+
+    # pool.close()
+    # pool.join()
+
+if __name__ == "__main__":
+    main()
+
+print("--- %s seconds ---" % (time.time() - start_time))
